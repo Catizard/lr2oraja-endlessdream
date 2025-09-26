8226220b7b7213b024b31c4019adb81220ed2bd6
Catizard <1185032459@qq.com>
2025-09-26 12:35:30 +0800

    remove some unused imports

--- core/src/bms/player/beatoraja/MainController.java
+++ core/src/bms/player/beatoraja/MainController.java
@@ -9,7 +9,6 @@ import bms.player.beatoraja.exceptions.PlayerConfigException;
 import bms.player.beatoraja.modmenu.DownloadTaskMenu;
 import bms.player.beatoraja.modmenu.ImGuiNotify;
 import bms.player.beatoraja.modmenu.ImGuiRenderer;
-import bms.player.beatoraja.modmenu.SkinWidgetManager;
 import bms.player.beatoraja.modmenu.SongManagerMenu;
 import bms.tool.mdprocessor.HttpDownloadProcessor;
 import bms.tool.mdprocessor.HttpDownloadSource;
